9c2b8360390d572ce8d849bf20471256d53192a1
bricefriha <contact@bricefriha.dev>
2024-12-16 11:22:22 +0000

    opyihkkl[';h

---
+++ MCT_sample_issue_touchbehavior/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MCT_sample_issue_touchbehavior
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MCT_sample_issue_touchbehavior/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MCT_sample_issue_touchbehavior
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
